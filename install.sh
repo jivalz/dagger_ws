00f97dc6d1b6b6692a8514d20755d54b8a1644d5
jivalz <jivaldhingra@gmail.com>
2026-07-10 19:11:16 +0530

    fixed sh iter1

--- install.sh
+++ install.sh
@@ -12,15 +12,11 @@ sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # 2. Add ROS 2 repository
-if [ ! -f /etc/apt/sources.list.d/ros2.list ]; then
-    sudo apt install software-properties-common -y
-    sudo add-apt-repository universe -y
-    sudo apt update && sudo apt install curl -y
-    sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-else
-    echo "ROS 2 repository already exists. Skipping repository setup."
-fi
+sudo apt install software-properties-common -y
+sudo add-apt-repository universe -y
+sudo apt update && sudo apt install curl -y
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # 3. Install ROS 2 and Build Tools
 sudo apt update
